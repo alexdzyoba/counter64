bca2e204ffe5f3025e460b587a059105ed1e56b8
Alex Dzyoba <alex@dzyoba.com>
2019-10-30 23:22:44 +0300

    Initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexdzyoba/counter64
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var Counter uint64
+
+func counter(done chan bool) {
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			Counter++
+		}
+	}
+}
+
+func printer(done chan bool) {
+	ticker := time.Tick(1 * time.Second)
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker:
+			fmt.Printf("0x%016x\n", Counter)
+		}
+	}
+}
+
+func main() {
+	done := make(chan bool)
+	go counter(done)
+	go printer(done)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigs
+	done <- true
+}
